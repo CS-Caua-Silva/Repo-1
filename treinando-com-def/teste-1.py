7002131c96af9c8918a71a7a7aa5e046ef9c130c
Cauã Rodrigues da Silva <cauardasilva@gmail.com>
2026-01-12 18:21:45 -0300

    Adicionei mais uma pasta para usar a estrutura de repetição while no PY

---
+++ treinando-com-def/teste-1.py
@@ -0,0 +1,19 @@
+def calcular_imposto(valor):
+    if valor < 1000:
+        imposto = valor * 0.1
+
+    elif valor < 2000:
+        imposto = valor * 0.13
+
+    else:
+        imposto = valor * 0.2
+
+    return imposto
+
+preço_produto1 = 2500
+preço_produto2 = 3500
+imposto_produto1 = calcular_imposto(preço_produto1)
+imposto_produto2 = calcular_imposto(preço_produto2)
+
+print(imposto_produto1)
+print(imposto_produto2)
